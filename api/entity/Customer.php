e7f0601f903bb5adcb63fb2a288d145916fd5b5b
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-08-06 17:26:04 +0200

    Correction du merge

--- api/entity/Customer.php
+++ api/entity/Customer.php
@@ -1,6 +1,13 @@
 <?php
-
-class Customer  {
+class Customer implements JsonSerializable {
     public $id;
     public $name;
+
+    public function jsonSerialize()
+    {
+        return array(
+            'id'=>$this->id,
+            'name'=>$this->name,
+        );
+    }
 }
