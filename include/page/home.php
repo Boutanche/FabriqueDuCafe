45135624f474bc2df179cc663e4934746f41cf5f
Benoit BERTRAND <benoit.bertrand@live.fr>
2020-08-12 13:13:42 +0200

    Réparation Scotch et Truelle

--- include/page/home.php
+++ include/page/home.php
@@ -3,6 +3,24 @@
 include ('./include/layout/topbar.php');
 include ('./include/layout/sidebar.php');
 include ('./include/layout/heading.php');
+?>
+<div class="row container">
+    <div class="card">
+        <h6>Version V.0.1 : Codé à la truelle et avec les pieds : </h6>
+        <p>Par le stagiaire...</p>
+    </div>
+    <div class=" col-6">
+        <br>
+    </div>
+    <div class=" col-6">
+        <p>Tout n'est pas encore activé.</p>
+        <p> Vous pouvez uniquement : </p>
+        <ul>
+            <li>Créer une carte fidélité</li>
+            <li>Modifier cette carte</li>
+        </ul>
+    </div
+</div>
 
 
 
